31a487166803177b698fb3ef69c524700f9da6cc
wzmcarrick <wzmcarrick@gmail.com>
2022-11-12 19:32:28 -0500

    comment out dummy data

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,31 +1,31 @@
 import TaskItem from './TaskItem';
 
-const DUMMY_DATA = [
-  {
-    id: 1,
-    name: 'task1',
-    priority: 'High',
-    assignee: 'joseph',
-    dueDate: '2022-02-02',
-    // detail: 'lorem ipsum...lorem...............',
-  },
-  {
-    id: 2,
-    name: 'task2',
-    priority: 'Medium',
-    assignee: 'elton',
-    dueDate: '2022-03-02',
-    // detail: 'lorem ipsum...',
-  },
-  {
-    id: 3,
-    name: 'task1',
-    priority: 'Low',
-    assignee: 'ziming',
-    dueDate: '2023-02-02',
-    // detail: 'lorem ipsum...',
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: 1,
+//     name: 'task1',
+//     priority: 'High',
+//     assignee: 'joseph',
+//     dueDate: '2022-02-02',
+//     // detail: 'lorem ipsum...lorem...............',
+//   },
+//   {
+//     id: 2,
+//     name: 'task2',
+//     priority: 'Medium',
+//     assignee: 'elton',
+//     dueDate: '2022-03-02',
+//     // detail: 'lorem ipsum...',
+//   },
+//   {
+//     id: 3,
+//     name: 'task1',
+//     priority: 'Low',
+//     assignee: 'ziming',
+//     dueDate: '2023-02-02',
+//     // detail: 'lorem ipsum...',
+//   },
+// ];
 
 const Column = ({ data }) => {
   return data.map((task) => <TaskItem key={task.id} showTask={task} />);
